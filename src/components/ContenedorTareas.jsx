edabac8525bcfd7e2b8fb63efa6d6c6d08e8cb25
“iker777” <ikeralvarez777@gmail.com>
2025-06-10 13:21:30 +0200

    CSS tareas done

--- src/components/ContenedorTareas.jsx
+++ src/components/ContenedorTareas.jsx
@@ -3,6 +3,19 @@ import Tarea from "./Tarea";
 function ContenedorTareas(){
     return(
         <div className="contenedorTareas">
+            <h2 className="contenedorTareas__h2">Tareas por hacer</h2>
+            <Tarea/>
+            <Tarea/>
+            <Tarea/>
+            <Tarea/>
+            <Tarea/>
+            <Tarea/>
+            <Tarea/>
+            <Tarea/>
+            <Tarea/>
+            <Tarea/>
+            <Tarea/>
+            <Tarea/>
             <Tarea/>
         </div>
     )
--- src/components/Tarea.jsx
+++ src/components/Tarea.jsx
@@ -1,7 +1,14 @@
-function Tarea(){
-    return(
-        <div>
-            Tarea
+function Tarea() {
+    return (
+        <div className="tarea">
+            <div className="tarea__btnContainer">
+                <span className="tarea__btn tarea__btn--edit">🖊</span>
+                <span className="tarea__btn tarea__btn--exit">✖</span>
+            </div>
+            <div className="tarea__contenido">
+                <h2 className="tarea__h2">tituloTarea</h2>
+                <p className="tarea__p">textoTarea</p>
+            </div>
         </div>
     )
 }
